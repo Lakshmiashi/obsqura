b0f88a1638cb45c09571bd7fe87e1456e4c3454a
ASUS <ASUS@LAPTOP-QKE8DCG6>
2024-04-16 18:18:15 +0530

    sample for each class commit.

---
+++ Second/src/main/java/foreach/Egforeach.java
@@ -0,0 +1,16 @@
+package foreach;
+
+public class Egforeach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] arr1= {12,23,34,56,78};
+        for(int val:arr1)
+        {
+        	System.out.println(val);
+        }
+        }
+        
+	}
+
+
