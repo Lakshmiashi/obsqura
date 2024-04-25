038f02fd4a55670111e3a1ccd3f243d953d988a8
ASUS <ASUS@LAPTOP-QKE8DCG6>
2024-04-25 20:53:19 +0530

    Encapsulation

---
+++ first/src/main/java/Encapsulation/Class1.java
@@ -0,0 +1,14 @@
+package Encapsulation;
+
+public class Class1 {
+	private String name;
+	public void getName()
+	{
+		System.out.println(name);
+	}
+	public void setName(String name)
+	{
+	   this.name=name;
+	}
+
+}
---
+++ first/src/main/java/Encapsulation/Class2.java
@@ -0,0 +1,13 @@
+package Encapsulation;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class1 obj=new Class1();
+		obj.setName("Lakshmi");
+		obj.getName();
+
+	}
+
+}
