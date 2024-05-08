91a14b9fc42454ae5d11badd0ddec5b96ad124c0
ASUS <ASUS@LAPTOP-QKE8DCG6>
2024-05-08 22:57:01 +0530

    Encapsulatuion

--- first/src/main/java/Encapsulation/Class1.java
+++ first/src/main/java/Encapsulation/Class1.java
@@ -4,11 +4,11 @@ public class Class1 {
 	private String name;
 	public void getName()
 	{
-		System.out.println(name);
+		System.out.println(name);    //get used to get value ;
 	}
 	public void setName(String name)
 	{
-	   this.name=name;
+	   this.name=name;      //set method used to set value
 	}
 
 }
