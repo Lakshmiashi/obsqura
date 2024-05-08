a950694331f9ebb80975215b5c9b4b74d0e0761d
ASUS <ASUS@LAPTOP-QKE8DCG6>
2024-05-08 22:46:00 +0530

    Inheritance

---
+++ first/src/main/java/Inheritance/ParentA.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class ParentA {
+  
+	public void display1()
+	{
+		System.out.println("this is first parent");
+	}
+	
+}
---
+++ first/src/main/java/Inheritance/ParentB.java
@@ -0,0 +1,9 @@
+package Inheritance;
+
+public class ParentB extends ParentA {
+   
+	public void display2()
+	{
+		System.out.println("This is second parent");
+	}
+}
